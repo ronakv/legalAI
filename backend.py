83bb0cc9e46cde0c69645a0aba0549e880b64a5e
Ronak Vimadalal <ronakvimadalal@live.com>
2024-05-12 02:38:46 -0700

    trying to improve UI - chatGPT code

--- backend.py
+++ backend.py
@@ -1,84 +1,30 @@
-import boto3
-from botocore.client import Config
-from dotenv import load_dotenv
-import os
-load_dotenv()
+import streamlit as st
+import backend
+
+folder_path = "LFIDocs"
+#knowledge.upload_pdfs_to_pinecone(folder_path)
+
+# Set up the title of the app
+st.title('Indian Arbitration Robot')
 
 
-bedrock_config = Config(connect_timeout=120, read_timeout=120, retries={'max_attempts': 0})
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-bedrock_agent_client = boto3.client("bedrock-agent-runtime", region_name='us-west-2', config=bedrock_config,
-                                    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-                                    aws_secret_access_key=os.getenv('AWS_SECRET_KEY'))
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-boto3_session = boto3.session.Session(
-    region_name='us-west-2'
-)
-region_name = boto3_session.region_name
+if prompt := st.chat_input("Hello! Ask me an arbitration related question"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-model_id = "anthropic.claude-3-sonnet-20240229-v1:0"  # For testing with Claude Instant and Claude V2
-region_id = region_name
-
-
-def retrieveAndGenerate(input, kbId, sessionId=None, model_id="anthropic.claude-3-sonnet-20240229-v1:0",
-                        region_id="us-west-2"):
-    model_arn = f'arn:aws:bedrock:{region_id}::foundation-model/{model_id}'
-    if sessionId:
-        return bedrock_agent_client.retrieve_and_generate(
-            input={
-                'text': input
-            },
-            retrieveAndGenerateConfiguration={
-                'type': 'KNOWLEDGE_BASE',
-                'knowledgeBaseConfiguration': {
-                    'knowledgeBaseId': os.getenv('KBID'),
-                    'modelArn': model_arn
-                }
-            },
-            sessionId=sessionId
-        )
-    else:
-        return bedrock_agent_client.retrieve_and_generate(
-            input={
-                'text': input
-            },
-            retrieveAndGenerateConfiguration={
-                'type': 'KNOWLEDGE_BASE',
-                'knowledgeBaseConfiguration': {
-                    'knowledgeBaseId': kbId,
-                    'modelArn': model_arn
-                }
-            }
-        )
-
-
-def get_answer(query):
-    url_list = ''
-    query = query +" Provide case names and dates in text if the question is a legal one. Otherwise use the regular model without retrived context"
-    print('Querying:')
-    print(query)
-    response = retrieveAndGenerate(query, os.getenv('KBID'), model_id=model_id, region_id=region_id)
-
-    aws_region = "us-west-2"
-
-    # S3 bucket name
-    bucket_name = "legaldocuments-test"
-
-    # Initialize an empty list to hold all the S3 URLs
-    s3_urls = []
-
-    # Loop through each citation to extract S3 object URIs and create complete URLs
-    for citation in response['citations']:
-        for reference in citation['retrievedReferences']:
-            s3_uri = reference['location']['s3Location']['uri']
-            # Extract the object key from the S3 URI
-            object_key = s3_uri.split("/")[-1]
-            # Create the S3 URL using the specified format
-            s3_url = f"https://{bucket_name}.s3.{aws_region}.amazonaws.com/{object_key}"
-            s3_urls.append(s3_url)
-
-    # Print all the generated S3 URLs
-    for url in s3_urls:
-        url_list = url + "\n"
-
-    return response['output']['text'], url_list
+    with st.chat_message("assistant"):
+        stream, urls = backend.get_answer(prompt)
+        response = st.write(stream)
+        st.session_state.messages.append({"role": "assistant", "content": stream})
